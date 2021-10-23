ea55a43611a0e2306fc7e6a8082f376961faf767
Kazaght <92064007+Kazaght@users.noreply.github.com>
2021-10-23 20:00:38 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+let clave: string = "";
+let cantidadintentos: number = 1;
+while (clave !== "EUREKA" && cantidadintentos < 4) {
+  clave = prompt("Ingrese su clave");
+  if (clave === "EUREKA") {
+    console.log("Bienvenido");
+  } else {
+    console.log("Has agotado todos los intentos");
+    cantidadintentos++;
+  }
+}
